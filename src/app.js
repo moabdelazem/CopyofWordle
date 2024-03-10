a8d502b0b9161faa50728fd96f6c73327323407b
Mohamed Abdelazim <mabdelazemahmed@gmail.com>
2024-03-10 22:06:49 +0200

    Basic Structure Of MoWordle

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// Settings For Game Name
+const gameName = "MoWordle";
+
+// On Page Mounting
+document.addEventListener("DOMContentLoaded", () => {
+  // Set Game Name
+  document.title = gameName;
+  document.querySelector("h1").innerHTML = gameName;
+});
